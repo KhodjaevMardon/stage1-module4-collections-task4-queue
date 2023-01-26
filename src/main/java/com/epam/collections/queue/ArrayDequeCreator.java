ccb89ed8c70251755293437d9415a8f4f0ba362f
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-26 15:06:19 +0500

    finished tasks 1 and 3

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,33 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> resultDequeue = new ArrayDeque<>();
+        if (firstQueue.isEmpty()) {
+            throw new IllegalArgumentException("first queue is empty");
+        }
+        if (secondQueue.isEmpty()) {
+            throw new IllegalArgumentException("second queue is empty");
+        }
+
+        // driver code jala
+        resultDequeue.addLast(firstQueue.poll());
+        resultDequeue.addLast(firstQueue.poll());
+        resultDequeue.addLast(secondQueue.poll());
+        resultDequeue.addLast(secondQueue.poll());
+
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())) {
+            if (!firstQueue.isEmpty()) {
+                firstQueue.add(resultDequeue.pollFirst());
+                resultDequeue.addLast(firstQueue.poll());
+                resultDequeue.addLast(firstQueue.poll());
+            }
+            if (!secondQueue.isEmpty()) {
+                firstQueue.add(resultDequeue.pollFirst());
+                resultDequeue.addLast(secondQueue.poll());
+                resultDequeue.addLast(secondQueue.poll());
+            }
+        }
+
+        return resultDequeue;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> resultList = new ArrayList<>(),
+                helperList = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            helperList.add(i);
+        }
+        int index = everyDishNumberToEat - 1;
+        while(!helperList.isEmpty()) {
+            if (index >= helperList.size()) {
+                index = index % helperList.size();
+            }
+            resultList.add(helperList.get(index));
+            helperList.remove(index);
+            index--;
+            index += everyDishNumberToEat;
+        }
+
+        return resultList;
     }
 }
